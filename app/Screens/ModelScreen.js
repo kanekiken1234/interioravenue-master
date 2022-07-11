4d47102f027463edef4830308e846061e4d9be59
kanekiken1234 <ruthvikrokz@gmail.com>
2022-07-11 19:27:59 +0530

    .

--- app/Screens/ModelScreen.js
+++ app/Screens/ModelScreen.js
@@ -1,118 +1,25 @@
-// import React, {useState} from 'react';
-// import {StyleSheet} from 'react-native';
-// import {
-//   ViroARScene,
-//   ViroText,
-//   ViroConstants,
-//   ViroARSceneNavigator,
-//   Viro3DObject,
-//   ViroAmbientLight,
-//   ViroDirectionalLight,
-//   ViroBox,
-//   ViroNode,
-//   ViroSpotLight,
-//   ViroARPlaneSelector,
-// } from '@viro-community/react-viro';
-
-// function ModelScreen(props) {
-//   const [text, setText] = useState('Initializing AR...');
-//   const modelData = props.arSceneNavigator.viroAppProps.props.route.params;
-//   const{
-//     product_3D_model_mtl,
-//     product_3D_model_obj,
-//     product_3D_model_texture,
-//   } = modelData
-//   console.log("model data",modelData);
-
-//   function onInitialized(state, reason) {
-//     console.log('guncelleme', state, reason);
-//     if (state === ViroConstants.TRACKING_NORMAL) {
-//       setText('Hello World!');
-//     } else if (state === ViroConstants.TRACKING_NONE) {
-//       // Handle loss of tracking
-//     }
-//   }
-
-//   return (
-//     <ViroARScene onTrackingUpdated={onInitialized}>
-//       <ViroAmbientLight color="#FFFFFF" />
-//       <ViroDirectionalLight color="#FFFFFF" direction={[0, -1, -0.2]} />
-//       <ViroSpotLight
-//         innerAngle={5}
-//         outerAngle={90}
-//         direction={[0, 1, 0]}
-//         position={[0, -7, 0]}
-//         color="#FFFFFF"
-//         intensity={250}
-//       />
-//       <ViroARPlaneSelector>
-//         <ViroNode onDrag={() => {}} dragType="FixedToWorld">
-//           <Viro3DObject
-//             source={{
-//               uri: product_3D_model_obj
-//             }}
-//             resources={[
-//               {
-//                 uri: product_3D_model_mtl
-//               },
-//               {
-//                 uri: product_3D_model_texture
-//               }
-//               // {
-//               //   uri: 'https://ia-3d-models.s3.ap-south-1.amazonaws.com/models/Chairs/203/IKE160128_obj/maps/IKE160128_2_d.png',
-//               // },
-//             ]}
-//             position={[0.0, -1, -1]}
-//             scale={[0.001, 0.001, 0.001]}
-//             type="OBJ"
-//           />
-//         </ViroNode>
-//       </ViroARPlaneSelector>
-//     </ViroARScene>
-//   );
-// };
-
-// export default (props) => {
-//   return (
-//     <ViroARSceneNavigator
-//       autofocus={true}
-//       viroAppProps={{props}}
-//       initialScene={{
-//         scene: ModelScreen,
-//       }}
-//       style={styles.f1}
-//     />
-//   );
-// };
-
-// var styles = StyleSheet.create({
-//   f1: {flex: 1},
-//   helloWorldTextStyle: {
-//     fontFamily: 'Arial',
-//     fontSize: 30,
-//     color: '#ffffff',
-//     textAlignVertical: 'center',
-//     textAlign: 'center',
-//   },
-// });
-import React, {useState} from 'react';
-import {StyleSheet} from 'react-native';
+import React from 'react';
+import { StyleSheet } from 'react-native';
 import {
   ViroARScene,
-  ViroText,
   ViroConstants,
   ViroARSceneNavigator,
   Viro3DObject,
   ViroAmbientLight,
   ViroDirectionalLight,
-  ViroBox,
   ViroNode,
   ViroSpotLight,
-  ViroARPlaneSelector,
+  ViroARPlaneSelector
 } from '@viro-community/react-viro';
 
-const HelloWorldSceneAR = () => {
+const HelloWorldSceneAR = (props) => {
   const [text, setText] = useState('Initializing AR...');
+  console.log(props)
+  const modelData = props.arSceneNavigator.viroAppProps.objProps;
+  const {
+    resources, source
+  } = modelData
+  console.log("model data in ar screen", modelData);
 
   function onInitialized(state, reason) {
     console.log('guncelleme', state, reason);
@@ -125,14 +32,6 @@ const HelloWorldSceneAR = () => {
 
   return (
     <ViroARScene onTrackingUpdated={onInitialized}>
-      {/* <ViroAmbientLight color="#ffffff" />
-      <ViroDirectionalLight
-        color="#ffffff"
-        direction={[0, -1, -0.5]}
-        position={[0, 9, 0]}
-        castsShadow={true}
-        shadowOpacity={0.9}
-      /> */}
       <ViroAmbientLight color="#FFFFFF" />
       <ViroDirectionalLight color="#FFFFFF" direction={[0, -1, -0.2]} />
       <ViroSpotLight
@@ -144,62 +43,40 @@ const HelloWorldSceneAR = () => {
         intensity={250}
       />
       <ViroARPlaneSelector>
-        <ViroNode onDrag={() => {}} dragType="FixedToWorld">
-          {/* <Viro3DObject
-          source={require('./models/IKE060018_obj/IKEA-Ektorp_Armchair_Vallsta_Red-3D.obj')}
-          resources={[
-            require('./models/IKE060018_obj/IKE060018-3D.mtl'),
-            require('./models/IKE060018_obj/maps/IKE060018_01_d.jpg'),
-          ]}
-          position={[0.0, -1, -3]}
-          scale={[0.001, 0.001, 0.001]}
-          type="OBJ"
-        /> */}
-          {/* <Viro3DObject
-          source={require('./models/IKE060017_obj/IKEA-Allak_Swivel_Chair-3D.obj')}
-          resources={[
-            require('./models/IKE060017_obj/IKE060017-3D.mtl'),
-            // require('./models/IKE060018_obj/maps/IKE060018_01_d.jpg'),
-          ]}
-          position={[0.0, -1, -3]}
-          scale={[0.001, 0.001, 0.001]}
-          type="OBJ"
-        /> */}
+        <ViroNode onDrag={() => { }} dragType="FixedToWorld">
           <Viro3DObject
-            source={{
-              uri: 'https://ia-3d-models.s3.ap-south-1.amazonaws.com/models/Chairs/203/IKE160128_obj/IKE160128.obj',
-            }}
-            resources={[
-              {
-                uri: 'https://ia-3d-models.s3.ap-south-1.amazonaws.com/models/Chairs/203/IKE160128_obj/IKE160128.mtl',
-              },
-              {
-                uri: 'https://ia-3d-models.s3.ap-south-1.amazonaws.com/models/Chairs/203/IKE160128_obj/maps/IKE160128_1_d.jpg',
-              },
-              {
-                uri: 'https://ia-3d-models.s3.ap-south-1.amazonaws.com/models/Chairs/203/IKE160128_obj/maps/IKE160128_2_d.png',
-              },
-            ]}
+            source={source}
+            resources={resources}
             position={[0.0, -1, -1]}
             scale={[0.001, 0.001, 0.001]}
             type="OBJ"
           />
         </ViroNode>
       </ViroARPlaneSelector>
-      {/* //  <ViroBox
-      //   height={1}
-      //   length={1}
-      //   width={1}
-      //   position={[0, 0, -1]}
-      //   scale={[0.5, 0.5, 0.5]} /> */}
     </ViroARScene>
   );
 };
 
-export default () => {
+export default (props) => {
+
+  const objParams = props.route.params
+  let objProps = {};
+  let resources = { "uri": objParams.product_3D_model_mtl };
+  let source = { "uri": objParams.product_3D_model_obj };
+  let textureResource = objParams.product_3D_model_texture.map(each => {
+    return {
+      "uri": each
+    }
+  })
+  console.log([resources, ...textureResource])
+  objProps = {
+    resources: [resources, ...textureResource],
+    source: source
+  }
   return (
     <ViroARSceneNavigator
       autofocus={true}
+      viroAppProps={{ objProps }}
       initialScene={{
         scene: HelloWorldSceneAR,
       }}
@@ -209,7 +86,7 @@ export default () => {
 };
 
 var styles = StyleSheet.create({
-  f1: {flex: 1},
+  f1: { flex: 1 },
   helloWorldTextStyle: {
     fontFamily: 'Arial',
     fontSize: 30,
